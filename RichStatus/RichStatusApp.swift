1f3034cfba407e2621c2a39acf668a369156e2a0
Kamyar Sehati <kamyarsehati@icloud.com>
2023-01-08 23:44:59 +0000

    Initial Commit

---
+++ RichStatus/RichStatusApp.swift
@@ -0,0 +1,17 @@
+//
+//  RichStatusApp.swift
+//  RichStatus
+//
+//  Created by Kamyar on 08/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct RichStatusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
