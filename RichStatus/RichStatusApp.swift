7cf1bfd08b0aaa0909453f71f318833a353ff5d2
Kamyar Sehati <kamyarsehati@icloud.com>
2023-01-21 22:45:11 +0000

    update menu bars

--- RichStatus/RichStatusApp.swift
+++ RichStatus/RichStatusApp.swift
@@ -12,16 +12,38 @@ struct RichStatusApp: App {
 
     @ObservedObject var weatherKitManager = WeatherKitManager()
     @ObservedObject var locationManager = LocationManager()
+    @ObservedObject var timerManager = TimerManager()
     @State var cityName = LocationManager.AddressEnum.unknown
+    @State var timerState = TimerManager.TimerState.stoped
+
     var body: some Scene {
+        WindowGroup {
+            SettingsView()
+        }
+
         setupWeatherIcon()
-    private func setupWeatherIcon() -> MenuBarExtra<some View, Button<Text>> {
+        setupTimerIcon()
+    }
+
+    private func setupWeatherIcon() -> MenuBarExtra<some View, some View> {
+
         MenuBarExtra {
+            Button { } label: {
+                Image(systemName: cityName.image)
+                Text(cityName.string)
+            }.disabled(true)
+
+            Divider()
+            Button("Settings", action: {
+                NSApp.setActivationPolicy(.regular)
+            })
+
             Button("Quit", action: {
                 exit(-1)
             })
+
         } label: {
-            HStack {
+            HStack(spacing: 2) {
                 Image(systemName: weatherKitManager.symbol)
                 Text(weatherKitManager.temp)
             }
@@ -29,11 +51,57 @@ struct RichStatusApp: App {
                 NSApp.setActivationPolicy(.prohibited)
                 Timer.scheduledTimer(withTimeInterval: 60 * 15, repeats: true) { _ in
                     Task {
-//                        await weatherKitManager.getWeather(latitude: 52.49081, longitude: 1.88934)
+                        await self.updateWeather()
                     }
                 }
             }.task {
-//                await weatherKitManager.getWeather(latitude: 52.49081, longitude: 1.88934)
+                await updateWeather()
+            }
+        }
+    }
+
+    private func updateWeather() async {
+        self.cityName = await self.locationManager.getAddress(lat: self.locationManager.lat,
+                                                              long: self.locationManager.lon)
+        await self.weatherKitManager.getWeather(latitude: self.locationManager.lat,
+                                                longitude: self.locationManager.lon)
+    }
+
+    private func setupTimerIcon() -> MenuBarExtra<some View, some View> {
+        MenuBarExtra {
+            if timerManager.timerStat == .stoped ||
+                timerManager.timerStat == .paused {
+                Button {
+                    self.timerManager.startTimer()
+                } label: {
+                    Image(systemName: "play")
+                    Text("Start")
+                }
+            }
+            if timerManager.timerStat == .counting {
+                Button {
+                    self.timerManager.timerStat = .stoped
+                    self.timerManager.timerString = "Timer"
+                    AppData.timerRemaining = 0
+                } label: {
+                    Image(systemName: "stop")
+                    Text("Stop")
+                }
+                Button {
+                    self.timerManager.timerStat = .paused
+                } label: {
+                    Image(systemName: "pause")
+                    Text("Pause")
+                }
+            }
+            Button("Quit", action: {
+                exit(-1)
+            })
+
+        } label: {
+            HStack {
+                Image(systemName: timerManager.timerStat.image)
+                Text(timerManager.timerString)
             }
         }
     }
---
+++ RichStatusTests/RSUserDefaultTests.swift
@@ -0,0 +1,23 @@
+//
+//  RSUserDefaultTests.swift
+//  RichStatusTests
+//
+//  Created by Kamyar on 17/01/2023.
+//
+
+import XCTest
+
+
+// MARK: - RSUserDefaultTests
+
+final class RSUserDefaultTests: XCTestCase {
+
+    
+// MARK: - Helpers
+    
+    
+}
+
+// MARK: - UserDefault
+
+
